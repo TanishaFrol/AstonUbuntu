caba5a5169a009360e41df8269362aa18a06b2c2
tanisha <tanyshkafr@mail.ru>
2023-06-05 01:43:46 +0300

    commit 2.0

--- src/main/java/app/AppServlet.java
+++ src/main/java/app/AppServlet.java
@@ -13,7 +13,8 @@ public class AppServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
         resp.getWriter().printf("<html><body>");
-        resp.getWriter().printf("<h1>How do you do</h1>");
+        resp.getWriter().printf("<h1>How do you do the things that you do?</h1>");
+        resp.getWriter().printf("<h1>No one I know could ever keep up with you</h1>");
         resp.getWriter().printf("</body></html>");
         resp.getWriter().close();
     }
