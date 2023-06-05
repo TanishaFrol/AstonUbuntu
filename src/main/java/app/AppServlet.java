0675ca597c286e5c5ef0a8b291aa18baf97b119b
tanisha <tanyshkafr@mail.ru>
2023-06-06 00:42:21 +0300

    commit 9.0

--- src/main/java/app/AppServlet.java
+++ src/main/java/app/AppServlet.java
@@ -14,7 +14,7 @@ public class AppServlet extends HttpServlet {
         resp.getWriter().printf("<html><body>");
         resp.getWriter().printf("<h1>How do you do the things that you do?</h1>");
         resp.getWriter().printf("<h1>No one I know could ever keep up with you</h1>");
-        resp.getWriter().printf("<h1>How do you do?</h1>");
+        /*resp.getWriter().printf("<h1>How do you do?</h1>");
         resp.getWriter().printf("<h1>Did it ever make sense to you to say bye? (Bye bye)</h1>");
         resp.getWriter().printf("<h1>I see you in that chair with perfect skin</h1>");
         resp.getWriter().printf("<h1>Well, how have you been, baby, livin′ in sin?</h1>");
@@ -23,7 +23,7 @@ public class AppServlet extends HttpServlet {
         resp.getWriter().printf("<h1>Well, here we are spending time in the louder part of town</h1>");
         resp.getWriter().printf("<h1>And it feels like everything′s surreal</h1>");
         resp.getWriter().printf("<h1>When I get old I will wait outside your house</h1>");
-        resp.getWriter().printf("<h1>Cause your hands have got the power meant to heal</h1>");
+        resp.getWriter().printf("<h1>Cause your hands have got the power meant to heal</h1>");*/
         resp.getWriter().printf("</body></html>");
         resp.getWriter().close();
     }
