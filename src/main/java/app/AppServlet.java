26ddc02e4f9a92dca95b05e901d204c15700711b
tanisha <tanushkafr@mal.ru>
2023-06-05 12:01:58 +0300

    commit 5.0

--- src/main/java/app/AppServlet.java
+++ src/main/java/app/AppServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 @WebServlet(name = "AppServlet", urlPatterns = "/home")
 public class AppServlet extends HttpServlet {
     @Override
@@ -19,6 +18,12 @@ public class AppServlet extends HttpServlet {
         resp.getWriter().printf("<h1>Did it ever make sense to you to say bye? (Bye bye)</h1>");
         resp.getWriter().printf("<h1>I see you in that chair with perfect skin</h1>");
         resp.getWriter().printf("<h1>Well, how have you been, baby, livin′ in sin?</h1>");
+        resp.getWriter().printf("<h1>Hey, I gotta know, did you say hello?</h1>");
+        resp.getWriter().printf("<h1>How do you do?(How do you do?)</h1>");
+        resp.getWriter().printf("<h1>Well, here we are spending time in the louder part of town</h1>");
+        resp.getWriter().printf("<h1>And it feels like everything′s surreal</h1>");
+        resp.getWriter().printf("<h1>When I get old I will wait outside your house</h1>");
+        resp.getWriter().printf("<h1>Cause your hands have got the power meant to heal</h1>");
         resp.getWriter().printf("</body></html>");
         resp.getWriter().close();
     }
