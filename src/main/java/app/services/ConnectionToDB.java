51ed7a3aca662fa9a7b1bf14bb27a0865b0cc182
tanisha <tanushkafr@mal.ru>
2023-06-16 01:41:45 +0300

    commit 38.0

--- src/main/java/app/services/ConnectionToDB.java
+++ src/main/java/app/services/ConnectionToDB.java
@@ -9,28 +9,18 @@ import java.util.logging.Logger;
 
 public class ConnectionToDB {
 
-    private static String url;
-    private static String name;
-    private static String password;
-
-    static {
-        FileInputStream fis;
-        Properties properties = new Properties();
-        try {
-            fis = new FileInputStream("src/main/resources/config.properties");
-            properties.load(fis);
-            url = properties.getProperty("db.host");
-            name = properties.getProperty("db.login");
-            password = properties.getProperty("db.password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public ResultSet connect(String statementStr) {
         Connection connection = null;
         ResultSet result = null;
         try {
+            FileInputStream fis;
+            Properties properties = new Properties();
+            fis = new FileInputStream("src/main/resources/config.properties");
+            properties.load(fis);
+            String url = properties.getProperty("db.host");
+            String name = properties.getProperty("db.login");
+            String password = properties.getProperty("db.password");
+
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, name, password);
             Statement statement = connection.createStatement();
