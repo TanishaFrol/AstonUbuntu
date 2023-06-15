43f2c8a6e83bb2f77baebb4d9db81ca7f71c05e0
tanisha <tanushkafr@mal.ru>
2023-06-15 21:58:21 +0300

    commit 29.0

--- src/main/java/app/OrderController.java
+++ src/main/java/app/OrderController.java
@@ -3,6 +3,7 @@ package app;
 import app.dto.OrderDTO;
 import app.services.OrderService;
 import netscape.javascript.JSObject;
+import org.json.JSONObject;
 
 
 import javax.servlet.ServletException;
@@ -19,12 +20,12 @@ public class OrderController extends HttpServlet {
     OrderService orderService = new OrderService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        JSONObject jsonObject =  new JSONObject(getOrderList());
         resp.setContentType("text/html;charset=UTF-8");
         /*resp.getWriter().printf("<html><body>");
         resp.getWriter().printf("<h1>Order list</h1>");*/
-
-        resp.getWriter().printf(getOrderList().toString());
+        resp.getWriter().print(jsonObject);
+        //resp.getWriter().printf(getOrderList().toString());
         /*resp.getWriter().printf("</body></html>");*/
         resp.getWriter().close();
     }
